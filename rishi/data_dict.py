c256bdf1107a84d580b297944cf312bc3292ef9d
rg <rishigoutam@users.noreply.github.com>
2022-02-23 17:21:53 -0800

    Add SaleType labels

--- rishi/data_dict.py
+++ rishi/data_dict.py
@@ -1,5 +1,7 @@
 """
 Helper module for getting descriptions/labels of Ames Housing features
+
+See: `data/data_description.txt`
 """
 
 import pandas as pd
@@ -23,3 +25,18 @@ MSSubClass = pd.Series({
     180: 'PUD - MULTILEVEL - INCL SPLIT LEV/FOYER',
     190: '2 FAMILY CONVERSION - ALL STYLES AND AGES'
 }, name="MSSubClass")
+
+# SaleType: Type of sale
+SaleType = pd.Series({
+    'WD ': 'Warranty Deed - Conventional',  # NOTE space in `WD `
+    'CWD': 'Warranty Deed - Cash',
+    'VWD': 'Warranty Deed - VA Loan',
+    'New': 'Home just constructed and sold',
+    'COD': 'Court Officer Deed/Estate',
+    'Con': 'Contract 15% Down payment regular terms',
+    'ConLw': 'Contract Low Down payment and low interest',
+    'ConLI': 'Contract Low Interest',
+    'ConLD': 'Contract Low Down',
+    'Oth': 'Other'
+}, name="SaleType")
+
