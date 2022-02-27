4cf9f7e4e479e8c99a240b98850e47a250fc8026
rg <rishigoutam@users.noreply.github.com>
2022-02-27 14:38:39 -0800

    Encode MoSold as categorical

--- rishi/data_dict.py
+++ rishi/data_dict.py
@@ -201,7 +201,20 @@ def convert_mssubclass(numerical_value):
 
 
 def convert_mosold(numerical_value):
-    return None
+    match numerical_value:
+        case 1: return "January"
+        case 2: return "February"
+        case 3: return "March"
+        case 4: return "April"
+        case 5: return "May"
+        case 6: return "June"
+        case 7: return "July"
+        case 8: return "August"
+        case 9: return "September"
+        case 10: return "October"
+        case 11: return "November"
+        case 12: return "December"
+        case _: raise KeyError(f"Invalid month: {numerical_value}")
 
 
 def get_pud_indicator(mssubclass: int):
