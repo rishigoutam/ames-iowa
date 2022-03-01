842f40c1e9d865f7212a67d4c2fd7b9f7ee0ef50
rg <rishigoutam@users.noreply.github.com>
2022-02-28 21:14:24 -0800

    Update list of features by data type
    - created list of ordinal, categorical, numerical, boolean features
    - fix BsmtExposure NANs bug
    - verify df columns are all in our list of features

--- rishi/data_dict.py
+++ rishi/data_dict.py
@@ -9,22 +9,22 @@ import pandas as pd
 
 # MSSubClass: Identifies the type of dwelling involved in the sale
 MSSubClass = pd.Series({
-    20: '1-STORY 1946 & NEWER ALL STYLES',
-    30: '1-STORY 1945 & OLDER',
-    40: '1-STORY W/FINISHED ATTIC ALL AGES',
-    45: '1-1/2 STORY - UNFINISHED ALL AGES',
-    50: '1-1/2 STORY FINISHED ALL AGES',
-    60: '2-STORY 1946 & NEWER',
-    70: '2-STORY 1945 & OLDER',
-    75: '2-1/2 STORY ALL AGES',
-    80: 'SPLIT OR MULTI-LEVEL',
-    85: 'SPLIT FOYER',
-    90: 'DUPLEX - ALL STYLES AND AGES',
-    120: '1-STORY PUD (Planned Unit Development) - 1946 & NEWER',
-    150: '1-1/2 STORY PUD - ALL AGES',
-    160: '2-STORY PUD - 1946 & NEWER',
-    180: 'PUD - MULTILEVEL - INCL SPLIT LEV/FOYER',
-    190: '2 FAMILY CONVERSION - ALL STYLES AND AGES'
+    '20': '1-STORY 1946 & NEWER ALL STYLES',
+    '30': '1-STORY 1945 & OLDER',
+    '40': '1-STORY W/FINISHED ATTIC ALL AGES',
+    '45': '1-1/2 STORY - UNFINISHED ALL AGES',
+    '50': '1-1/2 STORY FINISHED ALL AGES',
+    '60': '2-STORY 1946 & NEWER',
+    '70': '2-STORY 1945 & OLDER',
+    '75': '2-1/2 STORY ALL AGES',
+    '80': 'SPLIT OR MULTI-LEVEL',
+    '85': 'SPLIT FOYER',
+    '90': 'DUPLEX - ALL STYLES AND AGES',
+    '120': '1-STORY PUD (Planned Unit Development) - 1946 & NEWER',
+    '150': '1-1/2 STORY PUD - ALL AGES',
+    '160': '2-STORY PUD - 1946 & NEWER',
+    '180': 'PUD - MULTILEVEL - INCL SPLIT LEV/FOYER',
+    '190': '2 FAMILY CONVERSION - ALL STYLES AND AGES'
 }, name="MSSubClass")
 
 # SaleType: Type of sale
@@ -41,173 +41,245 @@ SaleType = pd.Series({
     'Oth': 'Other'
 }, name="SaleType")
 
-def convert_po_to_ex(categorical_value):
+
+# WARNING: match requires python 3.10
+def __convert_po_to_ex(categorical_value):
     """
     Gets the ordinal value for the given categorical value
     :param categorical_value: one of 'Ex', 'Gd', 'TA', 'Fa', 'Po'
     :return: numeric value from 5 to 1
     """
     match categorical_value:
-        case 'Ex': return 5
-        case 'Gd': return 4
-        case 'TA': return 3
-        case 'Fa': return 2
-        case 'Po': return 1
+        case 'Ex':
+            return 5
+        case 'Gd':
+            return 4
+        case 'TA':
+            return 3
+        case 'Fa':
+            return 2
+        case 'Po':
+            return 1
 
 
-# WARNING: match requires python 3.10
+def convert_overallqual(categorical_value):
+    return __convert_po_to_ex(categorical_value)
+
+
+def convert_overallcond(categorical_value):
+    return __convert_po_to_ex(categorical_value)
+
+
 def convert_exterqual(categorical_value):
-    return convert_po_to_ex(categorical_value)
+    return __convert_po_to_ex(categorical_value)
 
 
 def convert_extercond(categorical_value):
-    return convert_po_to_ex(categorical_value)
+    return __convert_po_to_ex(categorical_value)
 
 
 def convert_bsmtqual(categorical_value):
     if categorical_value == 'NoBasement':
         return 0
     else:
-        return convert_po_to_ex(categorical_value)
+        return __convert_po_to_ex(categorical_value)
 
 
 def convert_bsmtcond(categorical_value):
     if categorical_value == "NoBasement":
         return 0
     else:
-        return convert_po_to_ex(categorical_value)
+        return __convert_po_to_ex(categorical_value)
 
 
 def convert_bsmtexposure(categorical_value):
     match categorical_value:
-        case 'Gd': return 3
-        case 'Av': return 2
-        case 'Mn': return 1
-        case 'NoBasement': return 0
+        case 'Gd':
+            return 4
+        case 'Av':
+            return 3
+        case 'Mn':
+            return 2
+        case 'No':
+            return 1
+        case 'NoBasement':
+            return 0
 
 
 def convert_bsmtfintype(categorical_value):
     match categorical_value:
-        case 'GLQ': return 6
-        case 'ALQ': return 5
-        case 'BLQ': return 4
-        case 'Unf': return 1
-        case 'LwQ': return 2
-        case 'Rec': return 3
-        case 'NoBasement': return 0
+        case 'GLQ':
+            return 6
+        case 'ALQ':
+            return 5
+        case 'BLQ':
+            return 4
+        case 'Unf':
+            return 1
+        case 'LwQ':
+            return 2
+        case 'Rec':
+            return 3
+        case 'NoBasement':
+            return 0
 
 
 def convert_functional(categorical_value):
     match categorical_value:
-        case 'Typ': return 8
-        case 'Min1': return 7
-        case 'Min2': return 6
-        case 'Mod': return 5
-        case 'Maj1': return 4
-        case 'Maj2': return 3
-        case 'Sev': return 2
-        case 'Sal': return 1
+        case 'Typ':
+            return 8
+        case 'Min1':
+            return 7
+        case 'Min2':
+            return 6
+        case 'Mod':
+            return 5
+        case 'Maj1':
+            return 4
+        case 'Maj2':
+            return 3
+        case 'Sev':
+            return 2
+        case 'Sal':
+            return 1
 
 
 def convert_fireplacequ(categorical_value):
     if categorical_value == "NoFireplace":
         return 0
     else:
-        return convert_po_to_ex(categorical_value)
+        return __convert_po_to_ex(categorical_value)
 
 
 def convert_heatingqc(categorical_value):
-    return convert_po_to_ex(categorical_value)
+    return __convert_po_to_ex(categorical_value)
 
 
 def convert_kitchenqual(categorical_value):
-    return convert_po_to_ex(categorical_value)
+    return __convert_po_to_ex(categorical_value)
 
 
 def convert_landslope(categorical_value):
     match categorical_value:
-        case 'Gtl': return 3
-        case 'Mod': return 2
-        case 'Sev': return 0
+        case 'Gtl':
+            return 3
+        case 'Mod':
+            return 2
+        case 'Sev':
+            return 0
 
 
 def convert_lotshape(categorical_value):
     match categorical_value:
-        case 'Reg': return 4
-        case 'IR1': return 3
-        case 'IR2': return 2
-        case 'IR3': return 1
+        case 'Reg':
+            return 4
+        case 'IR1':
+            return 3
+        case 'IR2':
+            return 2
+        case 'IR3':
+            return 1
 
 
 def convert_garagecond(categorical_value):
     if categorical_value == "NoGarage":
         return 0
     else:
-        return convert_po_to_ex(categorical_value)
+        return __convert_po_to_ex(categorical_value)
 
 
 def convert_garagequal(categorical_value):
     if categorical_value == "NoGarage":
         return 0
     else:
-        return convert_po_to_ex(categorical_value)
+        return __convert_po_to_ex(categorical_value)
 
 
 def convert_street(categorical_value):
     match categorical_value:
-        case 'Pave': return 2
-        case 'Grvl': return 1
+        case 'Pave':
+            return 2
+        case 'Grvl':
+            return 1
 
 
 def convert_paveddrive(categorical_value):
     match categorical_value:
-        case 'Y': return 3
-        case 'P': return 2
-        case 'N': return 1
+        case 'Y':
+            return 3
+        case 'P':
+            return 2
+        case 'N':
+            return 1
 
 
 def convert_alley(categorical_value):
     match categorical_value:
-        case 'Pave': return 2
-        case 'Grvl': return 1
-        case 'NoAlley': return 0
+        case 'Pave':
+            return 2
+        case 'Grvl':
+            return 1
+        case 'NoAlley':
+            return 0
 
 
 def convert_utilities(categorical_value):
     match categorical_value:
-        case 'AllPub': return 4
-        case 'NoSewr': return 3
-        case 'NoSeWa': return 2
-        case 'ELO': return 1
+        case 'AllPub':
+            return 4
+        case 'NoSewr':
+            return 3
+        case 'NoSeWa':
+            return 2
+        case 'ELO':
+            return 1
 
 
 def convert_poolqc(categorical_value):
     match categorical_value:
-        case 'Ex': return 4
-        case 'Gd': return 3
-        case 'TA': return 2
-        case 'Fa': return 1
-        case 'NoPool': return 0  # PoolQC does not have a 'Po' category
+        case 'Ex':
+            return 4
+        case 'Gd':
+            return 3
+        case 'TA':
+            return 2
+        case 'Fa':
+            return 1
+        case 'NoPool':
+            return 0  # PoolQC does not have a 'Po' category
 
 
 def convert_mosold(numerical_value):
     match numerical_value:
-        case 1: return "January"
-        case 2: return "February"
-        case 3: return "March"
-        case 4: return "April"
-        case 5: return "May"
-        case 6: return "June"
-        case 7: return "July"
-        case 8: return "August"
-        case 9: return "September"
-        case 10: return "October"
-        case 11: return "November"
-        case 12: return "December"
-        case _: raise KeyError(f"Invalid month: {numerical_value}")
+        case 1:
+            return "January"
+        case 2:
+            return "February"
+        case 3:
+            return "March"
+        case 4:
+            return "April"
+        case 5:
+            return "May"
+        case 6:
+            return "June"
+        case 7:
+            return "July"
+        case 8:
+            return "August"
+        case 9:
+            return "September"
+        case 10:
+            return "October"
+        case 11:
+            return "November"
+        case 12:
+            return "December"
+        case _:
+            raise KeyError(f"Invalid month: {numerical_value}")
 
 
-def get_pud_indicator(mssubclass: int):
+def get_pud_indicator(mssubclass: str):
     """
     :param mssubclass: the dwelling type of the property
     :return: whether the property is in a PUD
@@ -215,7 +287,7 @@ def get_pud_indicator(mssubclass: int):
     return 'PUD' in MSSubClass[mssubclass]
 
 
-def get_num_floors(mssubclass):
+def get_num_floors(mssubclass: str):
     """
     :param mssubclass: the dwelling type of the property from the 16 choices of MSSubClass
     :return: number of floors in the dwelling type. some assumptions made for split, multi-level, and duplexes
@@ -238,7 +310,7 @@ def get_num_floors(mssubclass):
         raise KeyError(f"Invalid dwelling type detected: {dwelling}")
 
 
-def collapse_mssubclass(mssubclass: int):
+def collapse_mssubclass(mssubclass: str):
     """
     :param mssubclass: the dwelling type of the property from the 16 choices of MSSubClass
     :return: the collapsed subclass type: Traditional, Split, Duplex
