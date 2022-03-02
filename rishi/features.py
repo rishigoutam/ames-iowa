cadc1bf1c2c5f2eb41faade6315bbfe81d53b6b8
rg <rishigoutam@users.noreply.github.com>
2022-03-01 21:11:24 -0800

    create Age feature
    - refactor features module to use sets
    - drop collapsed/combined features
    - create Age feature (since last remodel/yearbuilt)

--- rishi/features.py
+++ rishi/features.py
@@ -1,5 +1,7 @@
 """
-Helper module for getting features by data type
+Helper module for getting features by data type.
+
+The features are created/dropped in `engineer.ipynb` and must be manually entered here
 """
 
 import pandas as pd
@@ -7,32 +9,38 @@ import pandas as pd
 guid = ['PID']
 target = ['SalePrice']
 
-ordinal = ['ExterQual', 'ExterCond', 'OverallCond', 'OverallQual',
-           'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2',
-           'Functional', 'FireplaceQu', 'HeatingQC', 'KitchenQual', 'LandSlope', 'LotShape',
-           'GarageCond', 'GarageQual', 'Street', 'PavedDrive', 'Alley', 'Utilities', 'PoolQC']
+__collapse_ordinal = set()
+__combine_ordinal = set()
+__drop_ordinal = set()
+__ordinal = {'ExterQual', 'ExterCond', 'OverallCond', 'OverallQual', 'BsmtQual', 'BsmtCond', 'BsmtExposure',
+           'BsmtFinType1', 'BsmtFinType2', 'Functional', 'FireplaceQu', 'HeatingQC', 'KitchenQual', 'LandSlope',
+           'LotShape', 'GarageCond', 'GarageQual', 'Street', 'PavedDrive', 'Alley', 'Utilities', 'PoolQC'}
+ordinal = list(__ordinal.union(__collapse_ordinal).union(__combine_ordinal) - __drop_ordinal)
 
 # TODO create more based on R^2. We want to create features only if we will use them
-collapse_categorical = ['Collapse_MSSubClass']
-categorical = ['MSSubClass', 'MSZoning',
+__collapse_categorical = {'Collapse_MSSubClass'}
+__combine_categorical = set()
+__drop_categorical = {'MSSubClass'}
+__categorical = {'MSSubClass', 'MSZoning',
                'LandContour', 'LotConfig', 'Neighborhood',
                'Condition1', 'Condition2', 'BldgType', 'HouseStyle',
                'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'Foundation', 'Heating',
                'CentralAir', 'Electrical', 'GarageType', 'GarageFinish', 'Fence', 'MiscFeature',
                'MoSold', 'YrSold',
-               'SaleType', 'SaleCondition']
-categorical = categorical + collapse_categorical
+               'SaleType', 'SaleCondition'}
+categorical = list(__categorical.union(__collapse_categorical).union(__combine_categorical) - __drop_categorical)
 
-new_numerical = ['NumFloors', 'BsmtAllBaths', 'AbvGrdBaths']
-numerical = ['GrLivArea', 'LotArea', 'LotFrontage', 'MasVnrArea',
+__new_numerical = {'NumFloors', 'BsmtAllBaths', 'AbvGrdBaths'}
+__drop_numerical = {'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'YearBuilt', 'YearRemodAdd'}
+__numerical = {'GrLivArea', 'LotArea', 'LotFrontage', 'MasVnrArea',
              'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF',
              '1stFlrSF', '2ndFlrSF', 'LowQualFinSF',
              'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr',
              'TotRmsAbvGrd', 'Fireplaces', 'GarageYrBlt', 'GarageCars', 'GarageArea',
              'WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea',
              'YearBuilt', 'YearRemodAdd',
-             'MiscVal']
-numerical = numerical + new_numerical
+             'MiscVal'}
+numerical = list(__numerical.union(__new_numerical) - __drop_numerical)
 
 # TODO create more based on R^2. We want to create features only if we will use them
 booleans = ['IsPUD']
@@ -49,11 +57,29 @@ def check_features(df: pd.DataFrame) -> None:
     print(f"{len(df.columns)} #columns == {len(guid + target + features)} #features+PID+SalePrice")
 
 
-def get_features(df: pd.DataFrame) -> pd.Series:
+def check_features2(df: pd.DataFrame) -> pd.Series:
     """
     :param df:
     :return: a series to manually check that the features are the same as in the df
     """
-    return pd.concat([pd.Series(sorted(features + guid + target), name="features"),
-                      pd.Series(sorted(df.columns), name="df.columns")],
+    return pd.concat([pd.Series(sorted(df.columns), name="df.columns"),
+                      pd.Series(sorted(features + guid + target), name="features")],
                      axis=1)
+
+def get_all_features():
+    return features
+
+
+def get_numerical_features():
+    return numerical
+
+def get_categorical_features():
+    return categorical
+
+
+def get_ordinal_features():
+    return ordinal
+
+
+def get_boolean_features():
+    return booleans
