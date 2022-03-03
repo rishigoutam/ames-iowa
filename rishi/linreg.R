d3f8905910eb4263181a0b4def9c4af2182fea77
rg <rishigoutam@users.noreply.github.com>
2022-03-02 20:38:42 -0800

    select features for multiple linear regression
    - select features in R based on p-value and vif
    - predict in python using selected features, get model beta coefficients

--- rishi/linreg.R
+++ rishi/linreg.R
@@ -1,33 +1,44 @@
 library(tidyverse)
 library(car)
 library(caret)
+library(corrplot)
 
 ames <- read.csv("./data/engineered.csv", stringsAsFactors = TRUE)
 
-colnames(ames)
+sort(colnames(ames))
 
 set.seed(9001)
-random_sample <- createDataPartition(ames$LogSalePrice, p = 0.7, list = FALSE)
+random_sample <- createDataPartition(ames$LogSalePrice, p = 0.7, list = TRUE)
 
 train <- ames[random_sample, ]
 test <- ames[-random_sample, ]
 
 model <- lm(LogSalePrice ~
-           GrLivArea + LotFrontage +
-           MSSubClass + BldgType +
-           OverallQual + Neighborhood +
-           SaleCondition,
-           data = train)
+              # Size
+              GrLivArea + AllBathrooms + BedroomAbvGr +
+              # Type
+              MSSubClass + MSZoning +
+              # Niceness
+              OverallQual + OverallCond + Neighborhood + KitchenQual +
+              # Sale
+              SaleCondition + YrSold +
+              # Features
+              CentralAir + Fireplaces +
+              # Others
+              IsNearNegativeCondition + LandContour,
+            data = train)
 
 # Verify model
 summary(model) # R2 of 0.8723 in train
 vif(model)
+# If we have multicolinearity, vif will tell us (aliased coefficients exist in the model)
 
+# FIX: this is failing due to test having a new MSSubClass factor level unseen in the train set
 predictions <- predict(model, test)
 
 R2(predictions, test$LogSalePrice) # 0.8587439 in test
 RMSE(predictions, test$LogSalePrice)
 MAE(predictions, test$LogSalePrice)
 
-ames[2577, 'LogSalePrice'] # predicted 12.36687, actual 12.27839
-ames[2577, 'SalePrice'] # predicted 234888.73, actual 215000
+ames[2577, 'LogSalePrice'] # actual 12.27839
+ames[2577, 'SalePrice'] # actual 215000
