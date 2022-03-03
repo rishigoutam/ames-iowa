1a4ba7a8268f2f27e4247ff4a649ee785dba4392
SacredFisher <srikar.pamidi@gmail.com>
2022-03-03 10:58:25 -0600

    Cleaning and Tidying data

--- srikar/timeseries.R
+++ srikar/timeseries.R
@@ -3,4 +3,24 @@ library(ggcorrplot)
 library(gtools)
 library(lubridate)
 
+#Acquire data 
+data <- read_csv("../data/engineered.csv")
+data <- data[-1]
+
+
+#Make Tidy average data frames 
+#First get neighborhood and price columns, along with sale date
+data$YrSold <- gsub("Yr_","", data$YrSold) #Clean date
+data <- data %>% select(YrSold, MoSold, Neighborhood, SalePrice)
+
+#Make a lubridate column 
+data$date <-paste(data$MoSold,sep="-", data$YrSold)
+data$date <-lubridate::my(data$date)
+data <- data %>% select(-c(YrSold,MoSold))
+
+data %>% select(Neighborhood) %>% unique() %>% view()
+#We Get Average prices by group, and by date. 
+data %>% group_by(Neighborhood, date) %>% 
+  summarise_at(vars(SalePrice), list(AvPrice = mean))
+
 
